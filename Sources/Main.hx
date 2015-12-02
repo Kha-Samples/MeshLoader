9880240121c139e547d64147666daea3e8a25dd5
Lubos Lenco <lubos.lenco@gmail.com>
2015-12-02 10:34:31 +0100

    Kha update

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -1,10 +1,14 @@
 package;
 
-import kha.Starter;
+import kha.System;
 
 class Main {
 	public static function main() {
-		var starter = new Starter();
-		starter.start(new MeshLoader());
+		System.init("MeshLoader", 800, 600, init);
+	}
+
+	static function init() {
+		var game = new MeshLoader();
+		System.notifyOnRender(game.render);
 	}
 }
